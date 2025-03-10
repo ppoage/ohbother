5af7562bf5c38c021e1dd4f85b192cbf670f9995
ppoage <poagepatrick@gmail.com>
2025-03-09 21:32:43 -0700

    issue tracker throwing wheelci error

--- setup.py
+++ setup.py
@@ -15,9 +15,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/ohbother",
-    project_urls={
-        "Bug Tracker": "https://github.com/yourusername/ohbother/issues",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
